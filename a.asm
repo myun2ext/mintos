2456182e8155e0f4e403d50ff2f2bda8c012e69e
myun2 <myun2@nwhite.info>
2015-01-16 15:36:14 +0900

    protected mode start

--- a.asm
+++ a.asm
@@ -1,33 +1,45 @@
 [bits 16]
-MOV	AH, 0x06
-MOV	AL, 0x00
-MOV	BH, 0x00
-MOV	CH, 0x00
-MOV	CL, 0x00
-MOV	DH, 0x00
-MOV	DL, 0x00
-INT	0x10
+CALL	print_dot
 
-MOV	AH, 0x0E
-MOV	AL, 0x42
-MOV	BH, 0x00
-MOV	BL, 0x07
-INT	0x10
-
-
+; stack pointe setup
 MOV	AX, 0x9000
 MOV	SS, AX
 MOV	SP, 0xFFFC
 
 CALL	Setup_GDT
 
+; A20
+CALL	print_dot
 CALL	Enable_A20
 
+; Protected Mode
+CALL	print_dot
+CLI
+JMP	0x8:start_pmode
 
 HLT
 
 %include "gdt.inc"
 %include "a20.inc"
+
+print_dot:
+	MOV	AH, 0x0E	; Function
+	MOV	AL, 0x2e	; Charactor Code
+	MOV	BH, 0x00
+	MOV	BL, 0x07	; Color
+	INT	0x10
+	RET
+
+[BITS 32]
+start_pmode:
+	MOV	AX, 0x10
+	MOV	SS, AX
+	MOV	ES, AX
+	MOV	FS, AX
+	MOV	GS, AX
+	MOV	DS, AX
+	MOV	ESP, 90000h
+	RET
 
 TIMES 510 - ($ - $$) DB 0
 DW 0xAA55
